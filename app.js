d4efdd7441a39e7e65b46dee404fa13fd4bdef11
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-09-05 20:45:26 +0100

    Setup basic socket connection with client

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express()
 
+app.set("view engine", "ejs");
+app.use(express.static("public"))
+
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.render("index")
 })
 
-app.listen(5000, () => console.log("Server started"))
+const server = app.listen(5000, () => console.log("Server started"))
+
+const io = require("socket.io")(server);
+
+io.on("connection", socket => {
+    console.log("New socket connection")
+})
